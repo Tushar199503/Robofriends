d820402f1645392554845a01dcb3bdeb33939f6f
tushar <tushargupta.967@gmail.com>
2021-02-27 20:00:10 +0530

    border change

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -5,7 +5,7 @@ const SearchBox = ({searchchange})=> {
     return(
         <div className='pa2'>
        <input 
-       className='pa3 ba b--green bg-lightest-blue' 
+       className='pa3 ba b--blue bg-lightest-blue' 
        type='search' 
        placeholder='search robots'
        onChange={searchchange}
